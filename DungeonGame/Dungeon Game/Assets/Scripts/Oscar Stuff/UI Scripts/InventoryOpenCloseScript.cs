5e757f0943e37a36a96c798616cfac88fd4ca36d
OscarTulkens <43540793+OscarTulkens@users.noreply.github.com>
2020-06-27 20:26:54 +0200

    - Updated Inventory Behaviour

--- DungeonGame/Dungeon Game/Assets/Scripts/Oscar Stuff/UI Scripts/InventoryOpenCloseScript.cs
+++ DungeonGame/Dungeon Game/Assets/Scripts/Oscar Stuff/UI Scripts/InventoryOpenCloseScript.cs
@@ -17,6 +17,12 @@ public class InventoryOpenCloseScript : MonoBehaviour
 
     private InventoryVisualManagerScript _visualInventoryManager;
 
+    private bool _inventoryOpened = false;
+
+    public float MovementDistance = 0;
+
+    public GameObject _playerModelScreen = null;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,92 +35,115 @@ public class InventoryOpenCloseScript : MonoBehaviour
         if (CombatManagerScript.Instance != null)
         {
             CombatManagerScript.Instance.OnStartCombat += CloseInventory;
-            CombatManagerScript.Instance.OnStartCombat += DisableOpenButton;
+            CombatManagerScript.Instance.OnStartCombat += DisableButton;
         }
         if (TreasureManager.Instance != null)
         {
             TreasureManager.Instance.OnStartTreasure += CloseInventory;
-            TreasureManager.Instance.OnStartTreasure += DisableOpenButton;
-            TreasureManager.Instance.OnEndTreasure += EnableOpenButton;
+            TreasureManager.Instance.OnStartTreasure += DisableButton;
+            TreasureManager.Instance.OnEndTreasure += EnableButton;
         }
 
         _startPositionInventoryButton = _inventoryButton.transform.position;
         _inventoryScrollRect = GetComponent<ScrollRect>();
         _inventoryScrollRect.enabled = false;
-        _actionOnInventoryOpenDone += ActivateInventory;
-        _actionOnInventoryCloseDone += DeactivateInventory;
+        _actionOnInventoryOpenDone += ActivateItemWheel;
+        _actionOnInventoryCloseDone += DeactivateItemWheel;
 
-        CloseInventory();
-        EnableOpenButton();
+        CloseItemWheel();
+        EnableButton();
+        _playerModelScreen.transform.position = new Vector3(Screen.width + 10, 0);
+
     }
+
+    #region Inventory Events
 
     public void OpenInventory(object sender, EventArgs e)
     {
-        OpenInventory();
+        CancelOnGoingTweens();
+        OpenItemWheel();
+        OpenPlayerModelScreen();
     }
 
-    public void OpenInventory()
+    public void CloseInventory(object sender, EventArgs e)
     {
         CancelOnGoingTweens();
-        if (ControlScript.Instance != null) ControlScript.Instance.enabled = false;
+        CloseItemWheel();
+        ClosePlayerModelScreen();
+    }
+
+    #endregion
+
+    #region Item Wheel
+
+    private void ActivateItemWheel()
+    {
+        _inventoryScrollRect.enabled = true;
+    }
+
+    private void DeactivateItemWheel()
+    {
+        _visualInventoryManager.DeleteInventorySlots();
+    }
+
+    public void OpenItemWheel()
+    {
         _inventoryScrollRect.enabled = true;
         _onGoingTweens.Add(LeanTween.move(transform.gameObject, new Vector3(0, 0), InventoryOpenTime).setEaseOutQuint().setOnComplete(_actionOnInventoryOpenDone).id);
-        if (_visualInventoryManager!=null)
+        if (_visualInventoryManager != null)
         {
             _visualInventoryManager.CreateInventorySlots();
         }
     }
 
-    public void CloseInventory(object sender, EventArgs e)
+    public void CloseItemWheel()
     {
-        CloseInventory();
-    }
-
-    public void CloseInventory()
-    {
-        CancelOnGoingTweens();
         _inventoryScrollRect.enabled = false;
-        _onGoingTweens.Add(LeanTween.move(transform.gameObject, new Vector3(Screen.width + 10, 0), InventoryOpenTime).setOnComplete(_actionOnInventoryCloseDone).setEaseOutQuint().id);
+        _onGoingTweens.Add(LeanTween.move(transform.gameObject, new Vector3(0, -MovementDistance), InventoryOpenTime).setOnComplete(_actionOnInventoryCloseDone).setEaseOutQuint().id);
     }
 
+    #endregion
 
-    private void ActivateInventory()
+    #region Change Button
+
+    public void DisableButton(object sender, EventArgs e)
     {
-        _inventoryScrollRect.enabled = true;
+        DisableButton();
     }
 
-    private void DeactivateInventory()
+    public void DisableButton()
     {
-        _visualInventoryManager.DeleteInventorySlots();
-    }
-
-
-
-    public void DisableOpenButton(object sender, EventArgs e)
-    {
-        DisableOpenButton();
-    }
-
-    public void DisableOpenButton()
-    {
-        _onGoingTweens.Add(LeanTween.move(_inventoryButton, new Vector3(_inventoryButton.transform.position.x, -200), InventoryOpenTime / 2).setEaseOutExpo().id);
+        _onGoingTweens.Add(LeanTween.move(_inventoryButton, new Vector3(_inventoryButton.transform.position.x, -MovementDistance), InventoryOpenTime / 2).setEaseOutExpo().id);
         _inventoryButton.GetComponent<Button>().interactable = false;
     }
 
-
-
-    public void EnableOpenButton(object sender, EventArgs e)
+    public void EnableButton(object sender, EventArgs e)
     {
-        EnableOpenButton();
+        EnableButton();
     }
 
-    public void EnableOpenButton()
+    public void EnableButton()
     {
         _onGoingTweens.Add(LeanTween.move(_inventoryButton, _startPositionInventoryButton, InventoryOpenTime).setEaseOutExpo().id);
         _inventoryButton.GetComponent<Button>().interactable = true;
     }
 
+    private void MoveButton()
+    {
+        if (!_inventoryOpened)
+        {
+            _onGoingTweens.Add(LeanTween.move(_inventoryButton, new Vector3(_inventoryButton.transform.position.x, _startPositionInventoryButton.y + MovementDistance), InventoryOpenTime).setEaseOutQuint().id);
+        }
 
+        else if (_inventoryOpened)
+        {
+            _onGoingTweens.Add(LeanTween.move(_inventoryButton, _startPositionInventoryButton, InventoryOpenTime).setEaseOutQuint().id);
+        }
+    }
+
+    #endregion
+
+    #region Miscellaneous 
 
     public void ControlScriptOn(bool _bool)
     {
@@ -129,4 +158,49 @@ public class InventoryOpenCloseScript : MonoBehaviour
         }
     }
 
+    #endregion
+
+    #region Inventory Button Interaction
+    public void InventoryButtonPressed()
+    {
+        if (!_inventoryOpened)
+        {
+            CancelOnGoingTweens();
+            OpenItemWheel();
+            MoveButton();
+            OpenPlayerModelScreen();
+            ControlScriptOn(false);
+            _inventoryOpened = true;
+        }
+
+        else if (_inventoryOpened)
+        {
+            CancelOnGoingTweens();
+            CloseItemWheel();
+            MoveButton();
+            ClosePlayerModelScreen();
+            ControlScriptOn(true);
+            _inventoryOpened = false;
+        }
+    }
+    #endregion
+
+    #region Player Model Screen
+
+    private void OpenPlayerModelScreen()
+    {
+        _onGoingTweens.Add(LeanTween.move(_playerModelScreen, new Vector3(0, 0), InventoryOpenTime).setEaseOutQuint().id);
+    }
+
+    private void ClosePlayerModelScreen()
+    {
+        _onGoingTweens.Add(LeanTween.move(_playerModelScreen, new Vector3(Screen.width + 10, 0), InventoryOpenTime).setEaseOutQuint().id);
+    }
+
+    #endregion
+
+    private void SpriteSwap()
+    {
+
+    }
 }
